b8be6b39d5548e5e8a6d070db44156fbb4c6f5a9
Anna Ivanchenko <bubachka1990@gmail.com>
2026-02-18 00:18:54 +0200

    add fixed 2-form task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,60 @@
-console.log('Form');
+// !-----------------------------------------------------------
+// * Додавання стилів та розмітки для галереї
+import '../css/styles.css';
+import '../css/2-form.css';
+
+// !----------------------------------------------------------- 
+// * Ключ для localStorage
+const localStorageKey = 'feedback-form-state';
+
+// * Об'єкт поза функціями
+let formData = {
+  email: '',
+  message: '',
+};
+
+// * Отримуємо форму
+const form = document.querySelector('.feedback-form');
+
+// * Перевірка localStorage при завантаженні
+
+const savedData = localStorage.getItem(localStorageKey);
+
+if (savedData) {
+  formData = JSON.parse(savedData);
+
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+// ---------------------------------------------------
+// * Делегування події input
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (name === 'email' || name === 'message') {
+    formData[name] = value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  }
+});
+
+
+// ---------------------------------------------------
+// * Submit
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  // * очищення
+  localStorage.removeItem(localStorageKey);
+  formData = { email: '', message: '' };
+  form.reset();
+});
